0609c8dc740adb465f7d0f8929315c656f7d37d7
DevinSmoot <smootd@student.swosu.edu>
2015-10-14 14:20:22 -0500

    finished HW6. Updating files.

--- src/COMSC1033_HW6.java
+++ src/COMSC1033_HW6.java
@@ -13,85 +13,66 @@
  * 		  	Will end if score gets to 0
  */
 import java.util.Scanner;
+
 public class COMSC1033_HW6 {
 	public static void main(String[] args) {
-		// Round 1
+		//Define variables
+		int number1;
+		int number2;
+		int score = 0;
+		int hardnessOfQuestion = 10;
+		int studentAnswer;
+		int correctAnswer;
+		//State game name and rules
+		System.out.println ("Let's play an addition game!");
+		System.out.println ("There will be four rounds of increasing difficulty.");
+		System.out.println ("If a question is answered incorrectly the difficulty will decrease.");
+		
+		//Set up a for loop for all four rounds
+		for (int i = 0; i < 4; i++)
+		{
 		//	Generate 2 random numbers
-		int number1 = (int)(Math.random() *10);
-		System.out.println("Number1 is :" + number1 );
-		
-		int number2 = (int)(Math.random() *10);
-		System.out.println("Number2 is :" + number2 );
-		
-		int correctAnswer = number1 + number2;
-		
+		number1 = (int)(Math.random() * hardnessOfQuestion);
+		//System.out.println("Number1 is :" + number1 );
+		number2 = (int)(Math.random() * hardnessOfQuestion);
+		//System.out.println("Number2 is :" + number2 );
+		correctAnswer = number1 + number2;
 		//  Ask the user to add these two numbers together
-		System.out.println
-		("What is " + number1 + " + " + number2 + "?");
-		
+		System.out.println("\nWhat is " + number1 + " + " + number2 + "?");
 		//  Read in their response
 		Scanner input = new Scanner(System.in);
-		int studentAnswer = input.nextInt();
+		//Take input and validate that input is an integer
+		 do {
+			 while (!input.hasNextInt()) {
+		            System.out.println("That's not a number! Please enter a number.");
+		            input.next(); // this is important!
+		        }
+		        studentAnswer = input.nextInt();
+		    } while (studentAnswer <= 0);
 		//  Check if the answer was correct
-		//		IF correct
+		//	IF correct
 		if (studentAnswer == correctAnswer){
+			//State that answer is correct
 			System.out.println("Answer was correct");
+			//Add points
+			score += 2;
+			//Show score
+			System.out.println("Score: " + score);
+			//Increase hardness of the question
+			hardnessOfQuestion *= 10;
 		}else{
+			//State that answer is incorrect
 			System.out.println("Answer was not correct");
+			//Remove points
+			score -= 1;
+			//Show score
+			System.out.println("Score: " + score);
+			//Decrease hardness of the question
+			hardnessOfQuestion /= 10;
 		}
-			
-		
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
-		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		//			Make the next question easier
-		// Round 2
-		//	Generate 2 random numbers
-		//  Ask the user to add these two numbers together
-		//  Read in their response
-		//  Check if the answer was correct
-		//		IF correct
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
-		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		//			Make the next question easier
-		// Round 3
-		//	Generate 2 random numbers
-		//  Ask the user to add these two numbers together
-		//  Read in their response
-		//  Check if the answer was correct
-		//		IF correct
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
-		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		//			Make the next question easier
-		// Round 4
-		//	Generate 2 random numbers
-		//  Ask the user to add these two numbers together
-		//  Read in their response
-		//  Check if the answer was correct
-		//		IF correct
-		//			Tell them it was correct
-		//			Give them points
-		//			Make the next question harder
-		//		IF not correct
-		//			Tell them it was wrong
-		// 			Tell them the correct answer
-		//			Do not give them points
-		//			Make the next question easier
-
+	}
+	System.out.println ("\nThanks for playing!");
+	System.out.println ("Your final score is: " + score);
 	}
 
 }
